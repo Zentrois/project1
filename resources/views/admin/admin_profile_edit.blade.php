8fe22d319a5a0ea4ac89c782abbabcecb5347a74
zentrois <zentrois@gmail.com>
2024-02-03 06:32:16 +0800

    v37 - admin profile and image update part3

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 
 <div class="page-content">
     <div class="container-fluid">
@@ -10,7 +11,7 @@
 
                         <h4 class="card-title">Edit Profile Page</h4>
 
-                        <form action="">
+                        <form>
                             <div class="row mb-3">
                                 <label for="name" class="col-sm-2 col-form-label">Name</label>
                                 <div class="col-sm-10">
@@ -36,17 +37,17 @@
                             <!-- end row -->
 
                             <div class="row mb-3">
-                                <label for="" class="col-sm-2 col-form-label">Profile Image</label>
+                                <label for="image" class="col-sm-2 col-form-label">Profile Image</label>
                                 <div class="col-sm-10">
-                                    <input class="form-control" type="file" placeholder="Profile Image" name="profile_image" id="profile_image" value="">
+                                    <input class="form-control" type="file" placeholder="Profile Image" name="image" id="image" value="">
                                 </div>
                             </div>
                             <!-- end row -->
 
                             <div class="row mb-3">
-                                <label for="profile_image" class="col-sm-2 col-form-label"></label>
+                                <label class="col-sm-2 col-form-label"></label>
                                 <div class="col-sm-10">
-                                    <img class="rounded avatar-xl" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                                    <img id="showImage" class="rounded avatar-xl" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
                                 </div>
                             </div>
                             <!-- end row -->
@@ -63,6 +64,17 @@
     </div>
 </div>
 
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files[0]);
+        })
+    })
+</script>
 
     
 @endsection
